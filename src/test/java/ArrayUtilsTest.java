8d1533f8ac7fb22c6bf04048b352915eaa980f56
yeonjoon13 <77991998+yeonjoon13@users.noreply.github.com>
2025-03-29 00:43:34 -0400

    test

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -19,6 +19,45 @@ public class ArrayUtilsTest {
     }
 
 
+    @Test
+    public void testFindLastEmpty() {
+        // For any input where y appears in the second or later position, there is no error. Also,
+        // if x is empty, there is no error.
+        assertEquals(-1, ArrayUtils.findLast(new int[]{}, 3));
+    }
+
+    @Test
+    public void testFindLastNonExistent() {
+        assertEquals(-1, ArrayUtils.findLast(new int[]{1, 2}, 3));
+    }
+
+    @Test
+    public void testFindLastExistsFirstElement() {
+        assertEquals(0, ArrayUtils.findLast(new int[]{2, 3, 5}, 2));
+    }
+
+    @Test
+    public void testOddOrPosNull() {
+        // Do not execute fault
+        assertThrows(NullPointerException.class, () -> {ArrayUtils.oddOrPos(null);});
+    }
+
+
+    @Test
+    public void testOddOrPosAllPositives() {
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void testOddOrPositiveBothPositivesAndNegatives() {
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
+    }
+
+    @Test
+    public void testCountOfNull() {
+        assertThrows(NullPointerException.class, () -> ArrayUtils.countOf(null, 3));
+    }
+
 
 
 
